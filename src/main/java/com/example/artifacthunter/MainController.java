d5a5d041520017fa401e6352afcc3e3bc1f02e7b
Gladius4413 <95445509+Gladius4413@users.noreply.github.com>
2022-03-02 16:18:59 +0300

    test number 2

--- src/main/java/com/example/artifacthunter/MainController.java
+++ src/main/java/com/example/artifacthunter/MainController.java
@@ -9,6 +9,6 @@ public class MainController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("SteBullet is beutifull! 123131819984156g!");
+        welcomeText.setText("SteBullet is beutifull! 123131819984156g!, lead");
     }
 }
